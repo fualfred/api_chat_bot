de062d200452007d27f2558f49aaf21e7ca1d990
guoqifu <839114222@qq.com>
2025-07-07 00:04:35 +0800

    update

--- common/work_flow.py
+++ common/work_flow.py
@@ -15,7 +15,7 @@ class AgentState(TypedDict):
 
 
 def where_to_go(state: AgentState):
-    if state['messages'][-1].content == 'q':
+    if state['messages'][-1].content == 'reset':
         return 'reset_node'
     elif 'next_node' not in state['context']:
         return 'retriever_node'
@@ -40,7 +40,8 @@ def retriever_node(state: AgentState):
         commands.append(doc.page_content)
     state['context']['commands'] = commands
     state['context']['next_node'] = 'command_node'
-    state['messages'] = [AIMessage(content=commands)]
+    commands_str = "请输入对应接口的序号\n" + "\n".join([f"{i + 1}. {command}" for i, command in enumerate(commands)])
+    state['messages'] = [AIMessage(content=commands_str)]
     return state
 
 
@@ -66,13 +67,18 @@ def command_node(state: AgentState):
             state['context']['params_list'] = params_list
             state['context']['next_node'] = 'collection_params_node'
             state['context']['need_input_params'] = need_input_params
-            state['messages'] = [AIMessage(content=f"请输入参数{current_params_key}")]
+            value = need_input_params.get(current_params_key)
+            if isinstance(value, list):
+                value_str = "可选的值\n" + "\n".join(map(str, value))
+            else:
+                value_str = ""
+            state['messages'] = [AIMessage(content=f"请输入参数{current_params_key}的值,{value_str}")]
         else:
             state['context']['api_info'] = result_dict
             state['context']['request_body'] = request_body
             state['context']['next_node'] = 'http_node'
     except Exception as e:
-        state['messages'] = [AIMessage(content="请输入一个数字或者输入正确的数字")]
+        state['messages'] = [AIMessage(content=f"请输入一个数字或者输入正确的数字,异常{str(e)}")]
         state['context']['next_node'] = 'command_node'
         state['context']['err'] = str(e)
     return state
@@ -88,11 +94,17 @@ def collection_params_node(state: AgentState):
         state['context']['next_node'] = 'http_node'
     else:
         params_list = state['context']['params_list']
+        need_input_params = state['context']['need_input_params']
         current_params_key = params_list.pop()
         state['context']['request_body'] = request_body
         state['context']['current_params_key'] = current_params_key
         state['context']['params_list'] = params_list
-        state['messages'] = [AIMessage(content=f"请输入参数{current_params_key}")]
+        value = need_input_params.get(current_params_key)
+        if isinstance(value, list):
+            value_str = "可选的值\n" + "\n".join(map(str, value))
+        else:
+            value_str = ""
+        state['messages'] = [AIMessage(content=f"请输入参数{current_params_key}的值,{value_str}")]
     return state
 
 
